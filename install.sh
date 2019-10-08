e352fe44b38859c4a807a817d9038f1a92b83893
a696858 <francine.sauvage@atos.net>
2019-10-08 12:00:32 +0200

    openbmc directory added

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+export MISM_BULLSEQUANA_EDGE_VERSION=2.0.1
+
 ./install_awx.sh
 ./install_zabbix.sh
 
--- uninstall.sh
+++ uninstall.sh
@@ -5,34 +5,31 @@ docker-compose -f docker-compose-awx.yml down
 docker-compose -f docker-compose-zabbix.yml down
 
 echo "removing awx MISM images ...."
-docker image rm mism_awx_task
-docker image rm mism_awx_web
-docker image rm mism_rabbitmq
-docker image rm mism_awx_postgres
-docker image rm mism_pgadmin
-docker rmi `docker images |grep 'rabbitmq' |awk '{ print $3; }'`
-docker rmi `docker images |grep 'memcached' |awk '{ print $3; }'`
+docker image rm -f mism_awx_task
+docker image rm -f mism_awx_web
 docker image rm ansible/awx_task
 docker image rm ansible/awx_web
-docker image rm awx_postgres
-docker image rm zabbix-postgres
-docker image rm memcached
 docker image rm dpage/pgadmin4
+docker rmi -f `docker images |grep 'postgres' |awk '{ print $3; }'`
+docker rmi -f `docker images |grep 'memcached' |awk '{ print $3; }'`
+docker rmi -f `docker images |grep 'rabbitmq' |awk '{ print $3; }'`
 
 echo "removing zabbix MISM images ...."
-docker image rm mism_zabbix-server
-docker image rm mism_zabbix-web
-docker image rm mism_zabbix-agent
-docker image rm mism_zabbix-postgres
+docker image rm -f mism_zabbix-server
+docker image rm -f mism_zabbix-web
+docker image rm -f mism_zabbix-agent
+docker rmi -f `docker images |grep 'zabbix/zabbix-web-nginx-pgsql' |awk '{ print $3; }'`
+docker rmi -f `docker images |grep 'zabbix/zabbix-agent' |awk '{ print $3; }'`
+docker rmi -f `docker images |grep 'zabbix/zabbix-server-pgsql' |awk '{ print $3; }'`
 
 echo "removing dangling MISM containers (containers without images) ...."
 docker container prune --force
 echo "removing dangling MISM images (images without containers) ...."
 echo "do NOT care Errors if there is NO dangling image"
-docker rmi $(docker images |grep 'mism_zabbix-server')
-docker rmi $(docker images |grep 'mism_zabbix-web')
-docker rmi $(docker images |grep 'mism_zabbix-agent')
-docker rmi $(docker images |grep 'mism_zabbix-postgres')
+docker rmi -f $(docker images |grep 'mism_zabbix-server')
+docker rmi -f $(docker images |grep 'mism_zabbix-web')
+docker rmi -f $(docker images |grep 'mism_zabbix-agent')
+docker rmi -f $(docker images |grep 'mism_zabbix-postgres')
 docker image prune --force
 
 echo "removing ansible..."
