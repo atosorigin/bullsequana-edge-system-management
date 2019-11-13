3f303d868a2ffb645fb69df2c4b66f7fcecc8348
frsauvage <francine.sauvage@atos.net>
2019-11-13 16:35:24 +0100

    installer fomr internet

---
+++ install_zabbix_from_tar_files.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+chmod uo+w zabbix/server/externalscripts/openbmc
+
+set -euo pipefail
+
+if filename=$(readlink /etc/localtime); then
+    # /etc/localtime is a symlink as expected
+    export timezone=${filename#*zoneinfo/}
+    if [[ $timezone = "$filename" || ! $timezone =~ ^[^/]+/[^/]+$ ]]; then
+        # not pointing to expected location or not Region/City
+        >&2 echo "$filename points to an unexpected location"
+        exit 1
+    fi
+else  # compare files by contents
+    # https://stackoverflow.com/questions/12521114/getting-the-canonical-time-zone-name-in-shell-script#comment88637393_12523283
+     export timezone=$(find /usr/share/zoneinfo -type f ! -regex ".*/Etc/.*" -exec cmp -s {} /etc/localtime \; -print | sed -e 's@.*/zoneinfo/@@' | head -n1)
+fi
+
+echo $timezone
+
+export NO_PROXY=$NO_PROXY
+export HTTP_PROXY=$HTTP_PROXY
+export HTTPS_PROXY=$HTTPS_PROXY
+
+echo "starting BullSequana Edge Zabbix containers ...."
+docker-compose -f docker-compose-zabbix.yml up -d
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo "Zabbix is available on https://localhost:4443"
+echo "for more info, refer to documentation"
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+
+
+
