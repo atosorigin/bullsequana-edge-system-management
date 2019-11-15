b2158b3dbb0180fa5b3f3760c635e47777befc45
frsauvage <francine.sauvage@atos.net>
2019-11-15 15:15:38 +0100

    install stand-alone

--- install_playbooks_and_plugins.sh
+++ install_playbooks_and_plugins.sh
@@ -8,18 +8,22 @@ cp -r ansible/inventory/ansible.cfg /etc/ansible/
 
 # ansible plugin inventory is copied in default shared directory /usr/share/ansible/plugins/inventory/
 # you can adapt it if you have another ansible plugin inventory directory
+mkdir /usr/share/ansible
+mkdir /usr/share/ansible/plugins
+mkdir /usr/share/ansible/plugins/inventory
 cp ansible/plugins/inventory/redfish_plugin_ansible_inventory.yml /usr/share/ansible/plugins/inventory/redfish_plugin_ansible_inventory.yml
-cp ansible/plugins/inventory/redfish_plugin_ansible_inventory.py  /usr/share/ansible/plugins/inventory//redfish_plugin_ansible_inventory.py
+cp ansible/plugins/inventory/redfish_plugin_ansible_inventory.py  /usr/share/ansible/plugins/inventory/redfish_plugin_ansible_inventory.py
 
 # ansible plugin callback is copied in default shared directory /usr/share/ansible/plugins/callback/
 # you can adapt it if you have another ansible plugin callback directory
+mkdir /usr/share/ansible/plugins/callback
 cp -r ansible/plugins/callback/ansible_stdout_compact_logger      /usr/share/ansible/plugins/callback/ansible_stdout_compact_logger
 
 # ansible plugin module is copied in default shared directory /usr/share/ansible/plugins/modules/
 # you can adapt it if you have another ansible plugin module directory
+mkdir /usr/share/ansible/plugins/modules
 mkdir /usr/share/ansible/plugins/modules/remote_management
 mkdir /usr/share/ansible/plugins/modules/remote_management/openbmc
 cp ansible/plugins/modules/remote_management/openbmc/atos_openbmc.py       /usr/share/ansible/plugins/modules/remote_management/openbmc/atos_openbmc.py
 cp ansible/plugins/modules/remote_management/openbmc/atos_openbmc_utils.py /usr/share/ansible/plugins/modules/remote_management/openbmc/atos_openbmc_utils.py
 
-./add_playbooks.sh
