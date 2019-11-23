6d9e5cbcb914777fde10573f66cc0e81302f9b9c
Francine SAUVAGE <francine.sauvage@gmail.com>
2019-11-23 09:03:24 +0100

    Update install_awx_from_atos_dockerhub.sh

--- install_awx_from_atos_dockerhub.sh
+++ install_awx_from_atos_dockerhub.sh
@@ -4,12 +4,7 @@ export NO_PROXY=$NO_PROXY
 export HTTP_PROXY=$HTTP_PROXY
 export HTTPS_PROXY=$HTTPS_PROXY
 
-export MISM_BULLSEQUANA_EDGE_VERSION=2.0.2
-export AWX_BULLSEQUANA_EDGE_VERSION=9.0.1
-export RABBITMQ_AWX_BULLSEQUANA_EDGE_VERSION=3.8.1-management
-export POSTGRES_AWX_BULLSEQUANA_EDGE_VERSION=12.0-alpine
-export PGADMIN_AWX_BULLSEQUANA_EDGE_VERSION=4.14
-export MEMCACHED_AWX_BULLSEQUANA_EDGE_VERSION=1.5.20-alpine
+./versions.sh
 
 echo "starting BullSequana Edge Ansible AWX containers ...."
 docker-compose -f docker_compose_awx_from_atos_dockerhub.yml up -d
