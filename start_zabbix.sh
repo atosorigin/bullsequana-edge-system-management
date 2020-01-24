76eec84fa5ee6f834e591a1c0ea9e3983c83fe8f
frsauvage <francine.sauvage@atos.net>
2020-01-24 09:23:20 +0100

    synchro with bitbucket 2.0.5

--- start_zabbix.sh
+++ start_zabbix.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+chmod uo+w zabbix/server/externalscripts/openbmc
+
+set -euo pipefail
+
+if filename=$(readlink /etc/localtime); then
+    # /etc/localtime is a symlink as expected
+    export timezone=${filename#*zoneinfo/}
+    if [[ $timezone = "$filename" || ! $timezone =~ ^[^/]+/[^/]+$ ]]; then
+        # not pointing to expected location or not Region/City
+        >&2 echo "$filename points to an unexpected location"
+        exit 1
+    fi
+else  # compare files by contents
+    # https://stackoverflow.com/questions/12521114/getting-the-canonical-time-zone-name-in-shell-script#comment88637393_12523283
+     export timezone=$(find /usr/share/zoneinfo -type f ! -regex ".*/Etc/.*" -exec cmp -s {} /etc/localtime \; -print | sed -e 's@.*/zoneinfo/@@' | head -n1)
+fi
+
+echo $timezone
+
 . ./proxy.sh
 . ./versions.sh
 
