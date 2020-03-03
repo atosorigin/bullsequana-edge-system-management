447717037ceb384fb11fb3e6ecc80b65e68cbdc2
frsauvage <francine.sauvage@atos.net>
2020-03-03 17:06:22 +0100

    removed  add_playbooks.sh  get_logs.sh get_redfish_nmap_hosts.sh  get_sensors.sh

--- add_playbooks.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-echo "adding playbooks ..."
-docker exec -it awx_web projects/add_playbooks.py
-
-echo -e "\033[32mif you get an error: check https://localhost/api/v2/ on the docker host and re-run this script\033[0m"
-
